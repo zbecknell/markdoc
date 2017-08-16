bac1c460099f1cad25f91ff79b348b9876f83ccd
Zachary Becknell <zach.becknell@alexlee.com>
2017-08-15 22:46:18 -0400

    Dropzone and image upload working

---
+++ src/markdoc/markdoc/Controllers/DocController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace markdoc.Controllers
+{
+    public class DocController : Controller
+    {
+        private readonly IHostingEnvironment _HostingEnvironment;
+
+        public DocController(IHostingEnvironment hostingEnvironment)
+        {
+            _HostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImageUpload(IFormFile file)
+        {
+            var imageName = file.Name;
+
+            var webRootPath = _HostingEnvironment.WebRootPath;
+            var contentRootPath = _HostingEnvironment.ContentRootPath;
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+            var filePath = Path.Combine(webRootPath, @"uploads", fileName);
+
+            if(file.Length > 0)
+            {
+                using(var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            var path = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}{HttpContext.Request.PathBase}/uploads/{fileName}";
+
+            return Ok(new { size = file.Length, path });
+        }
+    }
+}
---
+++ src/markdoc/markdoc/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+// Write your JavaScript code.
+function sayHello() {
+    var compiler = document.getElementById("compiler").value;
+    var framework = document.getElementById("framework").value;
+}
+//# sourceMappingURL=site.js.map
