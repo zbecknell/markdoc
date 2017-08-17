a3d65b337658568e885b7678c2d420765ed60802
zbecknell <zach.becknell@alexlee.com>
2017-08-17 00:00:18 -0400

    CRUD

--- src/markdoc/markdoc/Controllers/DocController.cs
+++ src/markdoc/markdoc/Controllers/DocController.cs
@@ -6,21 +6,60 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using markdoc.Data;
+using Realms;
 
 namespace markdoc.Controllers
 {
     public class DocController : Controller
     {
         private readonly IHostingEnvironment _HostingEnvironment;
+        private Realm _realm;
 
-        public DocController(IHostingEnvironment hostingEnvironment)
+        public DocController(IHostingEnvironment hostingEnvironment, Realm realm)
         {
             _HostingEnvironment = hostingEnvironment;
+            _realm = realm;
         }
 
         public IActionResult Index()
         {
-            return View();
+
+            var docs = _realm.All<Document>().ToList();
+
+            //if (!docs.Any()) 
+                //realm.Write(() => {realm.Add(new Document() { Title = "Test Doc" }); });
+            return View(docs);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Document());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Document doc){
+
+            _realm.Write(() => _realm.Add(doc));
+
+            return RedirectToAction("ViewDoc", new { doc.Id });
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id){
+            return View("Create", _realm.Find<Document>(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string id, Document doc){
+            _realm.Write(() => _realm.Add(doc, update: true));
+
+            return RedirectToAction("ViewDoc", new { doc.Id });
+        }
+
+        public IActionResult ViewDoc(string id){
+            return View(_realm.Find<Document>(id));
         }
 
         [HttpPost]
---
+++ src/markdoc/markdoc/Data/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace markdoc.Data
+{
+    public class Document : Realms.RealmObject
+    {
+        [Realms.PrimaryKey]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string Body
+        {
+            get;
+            set;
+        }
+
+        public string Title { get; set; }
+    }
+}
